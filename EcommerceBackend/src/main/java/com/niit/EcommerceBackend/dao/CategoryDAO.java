69d7b8c26bc5f81037862887e76e7cca8ec0b709
Rajesh Routray <rajeshroutray36@gmail.com>
2021-01-21 10:31:42 +0530

    backend setup

---
+++ EcommerceBackend/src/main/java/com/niit/EcommerceBackend/dao/CategoryDAO.java
@@ -0,0 +1,13 @@
+package com.niit.EcommerceBackend.dao;
+
+import java.util.List;
+
+import com.EcommerceBackend.model.Category;
+
+public interface CategoryDAO {
+	
+	//method
+	
+	List<Category> lists();
+
+}
---
+++ EcommerceBackend/src/main/java/com/niit/EcommerceBackend/daoImpl/CatergoryDAOImpl.java
@@ -0,0 +1,58 @@
+package com.niit.EcommerceBackend.daoImpl;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.EcommerceBackend.model.Category;
+import com.niit.EcommerceBackend.dao.CategoryDAO;
+
+
+@Repository("categoryDAO")
+public class CatergoryDAOImpl implements CategoryDAO{
+	
+	
+	
+	
+	
+	private static List<Category> categories= new ArrayList<>();
+	
+	static {
+	Category cat= new Category();
+	cat.setId(1);
+	cat.setName("Watches");
+	cat.setDescription("time is good");
+	cat.setImageURL("one.jpg");
+	categories.add(cat);
+	
+	cat=new Category();
+	cat.setId(2);
+	cat.setName("Cameras");
+	cat.setDescription("nice pic");
+	cat.setImageURL("two.jpg");
+	categories.add(cat);
+	
+	cat=new Category();
+	cat.setId(3);
+	cat.setName("Shoes");
+	cat.setDescription("good swag");
+	cat.setImageURL("third.jpg");
+	categories.add(cat);
+	
+	
+	}
+	
+	
+
+	@Override
+	public List<Category> lists() {
+		// TODO Auto-generated method stub
+		return categories;
+	}
+	
+	
+	
+	
+}

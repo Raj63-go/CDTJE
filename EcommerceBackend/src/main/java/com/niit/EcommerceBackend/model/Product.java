a30701ad0c4bc15ef51d5dc849c21810f86ad2bd
Rajesh Routray <rajeshroutray36@gmail.com>
2021-05-06 12:23:36 +0530

    backend modification

--- EcommerceBackend/src/main/java/com/niit/EcommerceBackend/model/Product.java
+++ EcommerceBackend/src/main/java/com/niit/EcommerceBackend/model/Product.java
@@ -8,6 +8,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Min;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="PRODUCT")
@@ -19,29 +26,40 @@ public class Product {
 	
 	private String code;
 	
+	@NotBlank(message="Please enter your name!")
 	private String name;
 	
+	@NotBlank(message="Please enter brand name!")
 	private String brand;
 	
+	@NotBlank(message="Please fill the description!")
+	@JsonIgnore
 	private String description;
 	
+	@Min(value=1,message="Price can't be less than 1")
 	@Column(name="unit_Price")
 	private String unitPrice;
 	
 	private int quantity;
 	
 	@Column(name="is_active")
+	@JsonIgnore
 	private boolean active;
 	
 	@Column(name="category_id")
+	@JsonIgnore
 	private int categoryId;
 	
 	@Column(name="supplier_id")
+	@JsonIgnore
 	private int supplierId;
 	
 	private int purchases;
 	
 	private int views;
+	
+	@Transient
+	private MultipartFile file;
 	
 	public Product() {
 		this.code="PRD" +UUID.randomUUID().toString().substring(22).toUpperCase();
@@ -152,6 +170,15 @@ public class Product {
 				+ views + "]";
 	}
 
+	public MultipartFile getFile() {
+		return file;
+	}
+
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+
+	
 	
 	
 	
